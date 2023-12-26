6d95990c2e2d91f50baa9809af41415441854765
prathu06 <114643032+prathu06@users.noreply.github.com>
2023-12-26 12:20:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const cors=require("cors");
+const mysql=require("mysql2");
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+const con=mysql.createConnection({
+host:"sql12.freesqldatabase.com",
+user:"sql12672802",
+password:"XZZnLW4Dmm",
+database:"sql12672802"
+});
+
+app.post("/save",(req,res)=>{
+let data=[req.body.name,req.body.company,req.body.pkg];
+console.log(data);
+let sql="insert into student values(?,?,?)";
+con.query(sql,data,(err,result)=>{
+if(err) res.send(err);
+else   res.send(result);
+});
+});
+app.listen(9000,()=>{console.log("ready @9000");})
